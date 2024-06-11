8f2452d526a44ba8d95e7264cd797ba6096efd1f
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-06-11 12:28:07 -0300

    Variáveis

---
+++ introducao_java/src/br/com/javastudies/Main.java
@@ -0,0 +1,20 @@
+package br.com.javastudies;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Guilherme";
+        System.out.println("Hello " + name);
+
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        float divisao = (float) a / b;
+        System.out.println("Somaaaa: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
+    }
+}
