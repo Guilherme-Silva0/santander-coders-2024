fc6edba8d54b70f530c25ad36f24169e81c5ed97
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-06-11 18:34:28 -0300

    Métodos

--- poo_java/src/Main.java
+++ poo_java/src/Main.java
@@ -10,6 +10,13 @@ public class Main {
         cachorro.peso = 27.5;
         cachorro.tamanhoDoRabo = 5;
 
-        System.out.println(cachorro);
+        cachorro.latir();
+        System.out.println("O cachorro pegou uma " + cachorro.pegar());
+
+        System.out.println("O cachorro está " + cachorro.interagir("carinho"));
+
+        System.out.println("O cachorro está " + cachorro.interagir("mandar dormir"));
+
+        System.out.println("O cachorro está " + cachorro.interagir("nada"));
     }
 }
