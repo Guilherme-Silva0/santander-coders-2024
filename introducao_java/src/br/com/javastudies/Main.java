40879f76eafbfe8e5d28f3b13ed53fac0f505c26
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-06-11 15:39:41 -0300

    Estruturas Condicionais

--- introducao_java/src/br/com/javastudies/Main.java
+++ introducao_java/src/br/com/javastudies/Main.java
@@ -2,14 +2,34 @@ package br.com.javastudies;
 
 public class Main {
     public static void main(String[] args) {
-        boolean fimDeSemana = false;
-        boolean fazendoSol = true;
+        int nota = -1;
+        String graduacao;
 
-        boolean vamosAPraia = fimDeSemana && fazendoSol;
+        if (nota >= 80) {
+            graduacao = "A";
+        } else if (nota < 80 && nota >= 70) {
+            graduacao = "B";
+        } else if (nota < 70 && nota >= 60) {
+            graduacao = "C";
+        } else if (nota < 60 && nota >= 0) {
+            graduacao = "D";
+        } else {
+            graduacao = "";
+        }
 
-        System.out.println("Vamos à praia? " + vamosAPraia);
+        switch (graduacao) {
+            case "A":
+            case "B":
+                System.out.println("Aluno aprovado");
+                break;
+            case "C":
+            case "D":
+                System.out.println("Aluno reprovado");
+                break;
+            default:
+                System.out.println("Nota inválida");
+                break;
+        }
 
-        String menssagem = fimDeSemana ? "É fim de semana" : "Não é fim de semana";
-        System.out.println(menssagem);
     }
 }
