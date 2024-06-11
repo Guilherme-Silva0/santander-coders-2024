cad844e84484f72670ae6a6b4a6acea11923e569
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-06-11 17:46:34 -0300

    Hello Word com Java

---
+++ poo_java/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        byte varByte = 127;
+
+        System.out.println(varByte);
+        // short -32768 a 32767
+        // char 0 a 65535
+        // int -2b a 2b
+        // long -9t a 9t
+
+        int varInt = 234566789;
+
+        System.out.println(varInt);
+    }
+}
