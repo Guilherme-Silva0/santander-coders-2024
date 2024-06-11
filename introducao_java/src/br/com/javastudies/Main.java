8ddad5c82b863674f0cdb6b5f61fe83f89c35412
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-06-11 16:29:50 -0300

    Vetores

--- introducao_java/src/br/com/javastudies/Main.java
+++ introducao_java/src/br/com/javastudies/Main.java
@@ -1,31 +1,44 @@
 package br.com.javastudies;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.TextStyle;
-import java.util.Locale;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String nome = "Guilherme";
+        // String[] letras = new String[5];
+        // letras[0] = "S";
+        // letras[1] = "D";
+        // letras[2] = "R";
+        // letras[3] = "G";
+        // letras[4] = "H";
 
-        // ISO 8601
-        LocalDate hoje = LocalDate.now();
-        Locale brasil = Locale.of("pt", "BR");
-        String diaSemana = hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil);
-        LocalDateTime agora = LocalDateTime.now();
-        String saudacao;
-        if (agora.getHour() >= 0 && agora.getHour() < 12) {
-            saudacao = "bom dia";
-        } else if (agora.getHour() >= 12 && agora.getHour() < 18) {
-            saudacao = "boa tarde";
-        } else if (agora.getHour() >= 18 && agora.getHour() < 24) {
-            saudacao = "boa noite";
-        } else {
-            saudacao = "olá";
+        // String[] letras = { "A", "B", "C", "T", "I" };
+
+        // for (int i = 0; i < letras.length; i++) {
+        // System.out.println(letras[i]);
+        // }
+
+        // System.out.println(Arrays.toString(letras));
+
+        int[] numeros = { 9, 10, 12, 25, 2 };
+        int maior = numeros[0];
+        int menor = numeros[0];
+        int media = 0;
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] > maior) {
+                maior = numeros[i];
+            }
+
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
+
+            media += numeros[i];
         }
 
-        System.out.printf("Olá, %s. Hoje é %s, %s.%n", nome, diaSemana, saudacao.toUpperCase());
+        System.out.println("Maior: " + maior);
+        System.out.println("Menor: " + menor);
+        System.out.println("Media: " + (media / numeros.length));
 
     }
 }
