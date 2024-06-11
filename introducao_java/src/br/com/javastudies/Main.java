3a4afa1601db69107d5966da10d302cd84fb2fbf
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-06-11 13:10:10 -0300

    Operadores Booleanos | Tabela Verdade

--- introducao_java/src/br/com/javastudies/Main.java
+++ introducao_java/src/br/com/javastudies/Main.java
@@ -2,19 +2,14 @@ package br.com.javastudies;
 
 public class Main {
     public static void main(String[] args) {
-        String name = "Guilherme";
-        System.out.println("Hello " + name);
+        boolean fimDeSemana = false;
+        boolean fazendoSol = true;
 
-        int a;
-        int b = 2;
-        a = 3;
-        int soma = a + b;
-        int subtracao = a - b;
-        int multiplicacao = a * b;
-        float divisao = (float) a / b;
-        System.out.println("Somaaaa: " + soma);
-        System.out.println("Subtração: " + subtracao);
-        System.out.println("Multiplicação: " + multiplicacao);
-        System.out.println("Divisão: " + divisao);
+        boolean vamosAPraia = fimDeSemana && fazendoSol;
+
+        System.out.println("Vamos à praia? " + vamosAPraia);
+
+        String menssagem = fimDeSemana ? "É fim de semana" : "Não é fim de semana";
+        System.out.println(menssagem);
     }
 }
